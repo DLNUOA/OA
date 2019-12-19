66650e5e44a1c4c8c0d0655ce358a37c162ba0a0
Appleweir <905284451@qq.com>
2019-12-19 16:04:08 +0800

    开启Mybatis驼峰命名

--- src/test/java/edu/dlnu/oa/OaApplicationTests.java
+++ src/test/java/edu/dlnu/oa/OaApplicationTests.java
@@ -1,11 +1,14 @@
 package edu.dlnu.oa;
 
+import edu.dlnu.oa.dept.pojo.Dept;
 import edu.dlnu.oa.mapper.Index;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class OaApplicationTests {
@@ -17,7 +20,11 @@ class OaApplicationTests {
     }
     @Test
     void get(){
-        System.out.print("-----------" + index.getCount());
+//        System.out.print("-----------" + index.getCount());
+        List<Dept> allDept = index.getAllDept();
+        for (Dept dept : allDept) {
+            System.out.println(dept.toString());
+        }
     }
 
 }
