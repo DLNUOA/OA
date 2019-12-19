c45b499dc2239ff80056bb718eb4351d4c50df0c
Appleweir <905284451@qq.com>
2019-12-19 16:44:00 +0800

    加载日志

--- src/main/java/edu/dlnu/oa/controller/IndexController.java
+++ src/main/java/edu/dlnu/oa/controller/IndexController.java
@@ -1,12 +1,16 @@
 package edu.dlnu.oa.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    Logger log = LoggerFactory.getLogger(this.getClass());
     @RequestMapping("/")
     public String index(){
+        log.info("Visit Index");
         return "index";
     }
 
