00a5cfb00c91bc17523394445be4f546f2ed86ae
zhanghan-999 <1352336272@qq.com>
2020-01-04 09:09:57 +0800

    Merge branch 'master' of https://github.com/DLNUOA/OA
           Conflicts:
          	src/main/java/edu/dlnu/oa/URLController/URLController.java
          0104提交

---
+++ src/test/java/edu/dlnu/oa/zhanghantest/TestConsumeService.java
@@ -0,0 +1,28 @@
+package edu.dlnu.oa.zhanghantest;
+
+import edu.dlnu.oa.asset.pojo.Consume;
+import edu.dlnu.oa.asset.service.ConsumeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.sql.Date;
+
+@SpringBootTest
+public class TestConsumeService {
+
+    @Resource
+    private ConsumeService service;
+    @Test
+    public  void testAddConsume()
+    {
+        Consume consume=new Consume(33,"dianshi","dian",2,"zhang",2, Date.valueOf("2019-12-13"),"info");
+        service.addConsume(consume);
+    }
+
+
+
+
+
+
+}
