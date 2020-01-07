a52c03ce8bf12c5b0b58d3065146052d61c5bf3f
zhanghan-999 <1352336272@qq.com>
2020-01-07 13:39:13 +0800

    010765

---
+++ src/test/java/edu/dlnu/oa/zhanghantest/TestAsset.java
@@ -0,0 +1,13 @@
+//初试练习使用 IntelliJ IDEA
+package edu.dlnu.oa.zhanghantest;
+
+import edu.dlnu.oa.asset.pojo.Asset;
+
+public class TestAsset {
+
+    public static void main(String[] args) {
+        Asset asset1=new Asset(1,"电脑","电器类","台", 3000.00,100);
+        System.out.println(asset1);
+    }
+
+}
---
+++ src/test/java/edu/dlnu/oa/zhanghantest/TestAssetMapper.java
@@ -0,0 +1,70 @@
+package edu.dlnu.oa.zhanghantest;
+
+import edu.dlnu.oa.asset.mapper.AssetMapper;
+import edu.dlnu.oa.asset.pojo.Asset;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+public class TestAssetMapper {
+    @Resource
+    private AssetMapper mapper;
+
+    @Test
+    public  void testUpdate()
+    {
+        Asset asset=new Asset(4,"hhhGpp","hhh","hhh",200,100);
+        mapper.update(asset);
+    }
+
+    @Test
+    public  void testUpdateInventory()
+    {
+
+        mapper.setNewInventory(2,10);
+    }
+
+
+    @Test
+    public  void testInsert()
+    {
+        Asset asset=new Asset("hhhtes222","hhh","hhh",200,100);
+        mapper.insert(asset);
+    }
+    @Test
+    public  void testDelete()
+    {
+        mapper.delete(8);
+    }
+
+    @Test
+    public  void testQueryById()
+    {
+        Asset asset=mapper.queryById(2);
+        System.out.println(asset);
+    }
+
+    @Test
+    public  void testQueryByName()
+    {
+        List<Asset> list=mapper.queryByName("平板");
+        for (Asset asset:list){
+            System.out.println(asset);
+        }
+    }
+
+    @Test
+    public  void testQuery()
+    {
+        List<Asset> list=mapper.query();
+        for (Asset asset:list){
+            System.out.println(asset);
+        }
+    }
+
+
+
+}
