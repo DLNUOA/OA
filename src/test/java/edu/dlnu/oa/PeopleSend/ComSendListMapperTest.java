8693ef7478a554b5d44c95d92ef1dac8301847d9
WuHan <905284451@qq.com>
2020-01-07 13:32:15 +0800

    OK

--- src/test/java/edu/dlnu/oa/PeopleSend/ComSendListMapperTest.java
+++ src/test/java/edu/dlnu/oa/PeopleSend/ComSendListMapperTest.java
@@ -1,38 +1,38 @@
-package edu.dlnu.oa.PeopleSend;
-
-import edu.dlnu.oa.peopleSend.mapper.ComSendListMapper;
-import edu.dlnu.oa.peopleSend.pojo.ComSendList;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
-
-/**
- * 外派记录
- */
-@SpringBootTest
-public class ComSendListMapperTest {
-
-    @Resource
-    private ComSendListMapper mapper;
-
-    @Test
-    public void query(){
-        List<ComSendList> list = mapper.query();
-        for(ComSendList csl : list) {
-            System.out.println(csl);
-        }
-    }
-//    @Test
-//    public void add() {
-//        Map<String,Object> csl = new ComSendList(1,1,"张三","中软");
-//        mapper.insert(csl);
+//package edu.dlnu.oa.PeopleSend;
 //
+//import edu.dlnu.oa.peopleSend.mapper.ComSendListMapper;
+//import edu.dlnu.oa.peopleSend.pojo.ComSendList;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import javax.annotation.Resource;
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * 外派记录
+// */
+//@SpringBootTest
+//public class ComSendListMapperTest {
+//
+//    @Resource
+//    private ComSendListMapper mapper;
+//
+//    @Test
+//    public void query(){
+//        List<ComSendList> list = mapper.query();
+//        for(ComSendList csl : list) {
+//            System.out.println(csl);
+//        }
 //    }
-    @Test
-    public void upatestate() {
-        mapper.update(1,2);
-    }
-}
+////    @Test
+////    public void add() {
+////        Map<String,Object> csl = new ComSendList(1,1,"张三","中软");
+////        mapper.insert(csl);
+////
+////    }
+//    @Test
+//    public void upatestate() {
+//        mapper.update(1,2);
+//    }
+//}
